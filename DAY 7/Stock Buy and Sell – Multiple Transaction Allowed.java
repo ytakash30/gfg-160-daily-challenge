b347be2d75d718d6bfd0cb8169b64e8ecc10e0ce
Akash Mali <113687093+ytakash30@users.noreply.github.com>
2025-04-10 00:07:28 +0530

    Add files via upload

---
+++ DAY 7/Stock Buy and Sell – Multiple Transaction Allowed.java
@@ -0,0 +1,18 @@
+/*Problem Statement:- The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock i at price[i], you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
+
+Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day.
+*/
+
+Problem Solution :-
+ 
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
